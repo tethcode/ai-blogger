f1566a52c7ee07c4ac3500bf3baeadfedee5a842
Emmanuel Yakubu <tethcode@gmail.com>
2025-04-21 02:49:07 +0100

    Update settings.py

--- blogger/settings.py
+++ blogger/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a9u#tfr18zphx%m0h#21nidul(4+ek!3h8170)w-i6$j$kobw5'
-
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-default-key-for-dev")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
